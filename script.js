d97f1fd752c913adc4631b4f3ed7daf6e18da325
Lakshmi Vara Prasad Kuna <43074474+klvp@users.noreply.github.com>
2021-12-19 00:51:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/** @format */
+
+// let slider1 = document.getElementById("slider1");
+
+// let output1 = document.getElementById("output1");
+
+// output1.innerHTML = slider1.value;
+// slider1.oninput = function () {
+//   output1.innerHTML = this.value;
+// };
+
+let slider = document.getElementsByTagName("input");
+
+// console.log(slider);
+Object.values(slider).forEach((element) => {
+  console.log(element);
+  element.addEventListener("change", function () {
+    console.log(element);
+    var x = this.value;
+    var color = "linear-gradient(90deg , blue" + x + "%, green" + x + "%)";
+    this.style.background = color;
+  });
+});
